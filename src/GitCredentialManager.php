c21e5580a62f64461c0c10fae3aa4628198b0731
Ruben civeira <rubenciveira@gmail.com>
2025-07-18 00:17:23 +0200

    chore: reset prev commit

--- src/GitCredentialManager.php
+++ src/GitCredentialManager.php
@@ -159,11 +159,10 @@ class GitCredentialManager {
         $parsedUrl = parse_url($repository);
         $host = $parsedUrl['host'];
         $credentialUrl = "https://oauth2:{$credential['token']}@{$host}";
-        if ($credential['provider'] === 'github' || $credential['provider'] === 'custom') {
-            $credentialUrl = "https://{$credential['token']}@{$host}";
-        //     // Para GitHub: https://token@github.com/user/repo.git
-        //     $credentialUrl = "https://RubenCiveira:{$credential['token']}@{$host}{$path}";
-        }
+        // if ($credential['provider'] === 'github' || $credential['provider'] === 'custom') {
+            //     // Para GitHub: https://token@github.com/user/repo.git
+        //    $credentialUrl = "https://oauth2:{$credential['token']}@{$host}";
+        //}
         // } elseif ($credential['provider'] === 'gitlab') {
         //     // Para GitLab: https://oauth2:token@gitlab.com/user/repo.git
         //     $credentialUrl = "https://oauth2:{$credential['token']}@{$host}{$path}";
