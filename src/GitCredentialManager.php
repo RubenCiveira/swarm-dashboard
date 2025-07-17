c894f416c45306e54bfd57367f981f9acc178db2
Ruben civeira <rubenciveira@gmail.com>
2025-07-18 00:13:26 +0200

    chore: preserve temp to debug

--- src/GitCredentialManager.php
+++ src/GitCredentialManager.php
@@ -183,9 +183,9 @@ class GitCredentialManager {
         EOT);
         // Limpieza
         register_shutdown_function(function () use ($gitConfigDir, $gitConfigFile, $tmpHome) {
-            @unlink($gitConfigDir);
-            @unlink($gitConfigFile);
-            @rmdir($tmpHome);
+            // @unlink($gitConfigDir);
+            // @unlink($gitConfigFile);
+            // @rmdir($tmpHome);
         });
 
         $env = [
